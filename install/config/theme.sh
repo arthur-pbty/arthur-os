37037cb6d533dbc516f29e7e6113d4c987bd6f61
Puechberty Arthur <arthur.puechberty@outlook.com>
2026-06-17 20:36:25 +0200

    add theme

--- install/config/theme.sh
+++ install/config/theme.sh
@@ -10,7 +10,7 @@ sudo mkdir -p /etc/chromium/policies/managed
 sudo chmod a+rw /etc/chromium/policies/managed
 
 # Set initial theme
-omarchy-theme-set "Tokyo Night"
+omarchy-theme-set "Rainynight"
 rm -rf ~/.config/chromium/SingletonLock # otherwise archiso will own the chromium singleton
 
 # Set specific app links for current theme
